81b99862263ed81184a7985628c3fa5b0c8b2779
João Victor Barbosa dos Santos <victorsantos.dev@outlook.com>
2026-08-01 15:22:50 -0300

    Feat: Cria model de estoque lendo regras do submodulo

---
+++ src/models/estoqueModel.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+// Consumindo o Submódulo 1
+const regrasPath = path.join(__dirname, '../../../contracts/regras_produtos.json');
+const regras = JSON.parse(fs.readFileSync(regrasPath, 'utf8'));
+let inventario = [
+    { sku: "PROD-1", qtd: 15 }
+];
+module.exports = {
+    obterEstoque: () => inventario,
+    verificarAlerta: (qtdAtual) => qtdAtual <= regras.alerta_estoque_baixo
+};
