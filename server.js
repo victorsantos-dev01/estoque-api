07c9d72ed923bb796b880d9b68aaaaa02440614f
João Victor Barbosa dos Santos <victorsantos.dev@outlook.com>
2026-08-01 15:18:58 -0300

    Feat: Integra rotas de usuário na API

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const userRoutes = require('./src/routes/userRoutes'); // NOVA LINHA
 const app = express();
 app.use(express.json());
+app.use('/api/usuarios', userRoutes); // NOVA LINHA
 app.listen(3000, () => console.log('API rodando na porta 3000'));
